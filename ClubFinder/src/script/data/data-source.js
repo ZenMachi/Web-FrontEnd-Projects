d3a64f07d56dfa1a551126007ea859e5c15eec6b
ZenMachi <alheta.matalarens@gmail.com>
2023-04-12 09:49:54 +0700

    modified:   ClubFinder/app.js 	modified:   ClubFinder/index.html 	modified:   ClubFinder/src/script/data/clubs.js 	modified:   ClubFinder/src/script/data/data-source.js 	modified:   ClubFinder/src/script/view/main.js

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,27 +1,14 @@
+import clubs from "./clubs.js";
+
+// ORIGINAL
+
 // function DataSource(onSuccess, onFailed) {
 //   this.onSuccess = onSuccess;
 //   this.onFailed = onFailed;
 // }
 
-class DataSource {
-  constructor(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-  }
-}
-
-DataSource.prototype.searchClub = function (keyword) {
-  const filteredClubs = clubs.filter(club => club.name.toUpperCase().include(keyboard.toUpperCase()));
-
-  if (filteredClubs.length) {
-    this.onSuccess(filteredClubs);
-  } else {
-    this.onFailed(`${keyword} is not found`)
-  }
-}
-
 // DataSource.prototype.searchClub = function (keyword) {
-//   const filteredClubs = clubs.filter( club => 
+//   const filteredClubs = clubs.filter( club =>
 //     club.name.toUpperCase().includes(keyword.toUpperCase()));
 
 //   if (filteredClubs.length) {
@@ -30,3 +17,42 @@ DataSource.prototype.searchClub = function (keyword) {
 //     this.onFailed(`${keyword} is not found`);
 //   }
 // };
+
+// CLASS
+
+// class DataSource {
+//   constructor(onSuccess, onFailed) {
+//     this.onSuccess = onSuccess;
+//     this.onFailed = onFailed;
+//   }
+// }
+
+// DataSource.prototype.searchClub = function (keyword) {
+//   const filteredClubs = clubs.filter(club => club.name.toUpperCase().include(keyboard.toUpperCase()));
+
+//   if (filteredClubs.length) {
+//     this.onSuccess(filteredClubs);
+//   } else {
+//     this.onFailed(`${keyword} is not found`)
+//   }
+// }
+
+// Promise
+
+class DataSource {
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter((club) =>
+        club.name.toUpperCase().includes(keyword.toUpperCase())
+      );
+
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
+}
+
+export default DataSource;
--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,15 +1,31 @@
-const main = () => {
-  const searchElement = document.querySelector('#searchElement');
-  const buttonSearchElement = document.querySelector('#searchButtonElement');
-  const clubListElement = document.querySelector('#clubList');
+import DataSource from "../data/data-source.js";
 
-  const onButtonSearchClicked = () => {
-    const dataSource = new DataSource(renderResult, fallbackResult);
-    dataSource.searchClub(searchElement.value);
+const main = () => {
+  const searchElement = document.querySelector("#searchElement");
+  const buttonSearchElement = document.querySelector("#searchButtonElement");
+  const clubListElement = document.querySelector("#clubList");
+
+  const onButtonSearchClicked = async () => {
+    //
+    // const dataSource = new DataSource(renderResult, fallbackResult);
+    // dataSource.searchClub(searchElement.value);
+
+    // Callback Method
+
+    // DataSource.searchClub(searchElement.value)
+    //   .then(renderResult)
+    //   .catch(fallbackResult);
+    // Async Await Method
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
   };
 
-  const renderResult = results => {
-    clubListElement.innerHTML = '';
+  const renderResult = (results) => {
+    clubListElement.innerHTML = "";
     results.forEach((club) => {
       // ES 5
       // const name = club.name;
@@ -17,10 +33,10 @@ const main = () => {
       // const description = club.description;
 
       //ES 6
-      const {name, fanArt, description} = club;
+      const { name, fanArt, description } = club;
 
-      const clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
+      const clubElement = document.createElement("div");
+      clubElement.setAttribute("class", "club");
 
       clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
           <div class="club-info">
@@ -31,10 +47,12 @@ const main = () => {
     });
   };
 
-  const fallbackResult = message => {
-    clubListElement.innerHTML = '';
+  const fallbackResult = (message) => {
+    clubListElement.innerHTML = "";
     clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
-  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
+
+export default main;
