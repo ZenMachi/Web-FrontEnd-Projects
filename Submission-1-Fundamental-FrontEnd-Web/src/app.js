514d9bbb0b21fe74c40095203d60c196b63d861b
ZenMachi <alheta.matalarens@gmail.com>
2023-05-11 13:43:06 +0700

    Submission-1-Fundamental-FrontEnd-Web/.gitignore 	Submission-1-Fundamental-FrontEnd-Web/dist/ 	Submission-1-Fundamental-FrontEnd-Web/package-lock.json 	Submission-1-Fundamental-FrontEnd-Web/package.json 	Submission-1-Fundamental-FrontEnd-Web/src/index.html 	Submission-1-Fundamental-FrontEnd-Web/src/scripts/ 	Submission-1-Fundamental-FrontEnd-Web/src/styles/ 	Submission-1-Fundamental-FrontEnd-Web/tailwind.config.js 	Submission-1-Fundamental-FrontEnd-Web/webpack.common.js 	Submission-1-Fundamental-FrontEnd-Web/webpack.dev.js 	Submission-1-Fundamental-FrontEnd-Web/webpack.prod.js

---
+++ Submission-1-Fundamental-FrontEnd-Web/src/app.js
@@ -0,0 +1,13 @@
+// index.js
+import "regenerator-runtime";
+// import "./styles/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
+// import "tailwindcss/base.css";
+// import "tailwindcss/components.css";
+// import "tailwindcss/utilities.css";
+import "./scripts/components/app-bar.js";
+// import "./scripts/components/search-bar.js";
+import main from "./scripts/main.js";
+
+document.addEventListener("DOMContentLoaded", main());
