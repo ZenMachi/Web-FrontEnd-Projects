ca12662f7e6b14bb754c3ddc8c326a69baf4b222
ZenMachi <alheta.matalarens@gmail.com>
2023-05-14 13:30:28 +0700

    DAY 2 Push notification (Need to do IndexedDB)

--- KADIN-PWA-Project/notes-app/src/js/utils/sw-register.js
+++ KADIN-PWA-Project/notes-app/src/js/utils/sw-register.js
@@ -1,14 +1,25 @@
+import { Workbox } from "workbox-window";
+import PushNotificationHelper from "./push-notification-helper";
+
 const swRegister = async () => {
   if (!("serviceWorker" in navigator)) {
     console.log("Service Worker not supported in the browser");
     return;
   }
+
   try {
-    await navigator.serviceWorker.register("sw.bundle.js");
+    const wb = new Workbox("/sw.bundle.js");
+    const registration = await wb.register();
+
+    // Initialize web push notification after service worker activated successfully
+    await PushNotificationHelper.init({
+      subscribeButton: document.getElementById("subscribeBtn"),
+      unsubscribeButton: document.getElementById("unsubscribeBtn"),
+    });
+
     console.log("Service worker registered");
   } catch (error) {
-    console.log("Failed to Register service worker");
-    console.error(error);
+    console.error("Failed to register service worker", error);
   }
 };
 
