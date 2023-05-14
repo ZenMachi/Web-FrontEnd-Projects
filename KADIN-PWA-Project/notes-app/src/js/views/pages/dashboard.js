1b1bd188668c6fed246ad81696220ea1ec76bb67
ZenMachi <alheta.matalarens@gmail.com>
2023-05-14 15:10:02 +0700

    SESI 4 Native Capabilities

--- KADIN-PWA-Project/notes-app/src/js/views/pages/dashboard.js
+++ KADIN-PWA-Project/notes-app/src/js/views/pages/dashboard.js
@@ -1,5 +1,11 @@
-import NotesApi from '../../networks/notes-api';
-import { createNoteListEmptyTemplate, noteItemTemplate } from '../templates/template-creator';
+import BookmarkNotesIdb from "../../data/bookmark-notes-idb";
+import NotesApi from "../../networks/notes-api";
+import {
+  createBookmarkButtonTemplate,
+  createNoteListEmptyTemplate,
+  createRemoveBookmarkButtonTemplate,
+  noteItemTemplate,
+} from "../templates/template-creator";
 
 const Dashboard = {
   async render() {
@@ -13,41 +19,53 @@ const Dashboard = {
   },
 
   async afterRender() {
-    console.log('dashboard page');
+    console.log("dashboard page");
 
     this._initialData();
   },
 
   async _initialData() {
+    //* Get all bookmarked notes data from IndexedDB
+    const allBookmarkedNotes = await BookmarkNotesIdb.getAllBookmarkedNotes();
+
     // Get all notes data from API
     const notes = await NotesApi.getAll();
 
     // Get notesList element
-    const notesListEl = document.getElementById('notesList');
+    const notesListEl = document.getElementById("notesList");
 
     // Check if notes data is empty
     if (!notes.data.length) {
       return this._populateNotesListEmpty(notesListEl);
     }
 
-    this._populateNotesList(notesListEl, notes);
+    this._populateNotesList(notesListEl, notes, allBookmarkedNotes);
   },
 
-  _populateNotesList(containerEl, notes) {
-    containerEl.innerHTML = '';
+  _populateNotesList(containerEl, notes, allBookmarkedNotes) {
+    containerEl.innerHTML = "";
 
     // Populate notes list with note item template
     notes.data.forEach((note) => {
+      //* Check if note is bookmarked
+      const isBookmarked = allBookmarkedNotes.find(
+        (bookmarkedNotes) => bookmarkedNotes.id === note.id
+      );
+
+      const bookmarkButton = isBookmarked
+        ? createRemoveBookmarkButtonTemplate(note.id)
+        : createBookmarkButtonTemplate(note.id);
+
       containerEl.innerHTML += `
         <div class="col-12">
-          ${noteItemTemplate(note)}
+          ${noteItemTemplate(note, bookmarkButton)}
         </div>
       `;
     });
 
     // Add event listener to delete button for each note item
     containerEl.querySelectorAll(`#deleteNoteButton`).forEach((el) => {
-      el.addEventListener('click', async (event) => {
+      el.addEventListener("click", async (event) => {
         const noteId = event.target.dataset.id;
 
         try {
@@ -57,6 +75,41 @@ const Dashboard = {
           this._initialData();
         } catch (error) {
           console.log(error);
+        }
+      });
+    });
+
+    //* Add event listener to delete button for each note item
+    containerEl.querySelectorAll(`#bookmarkButton`).forEach((el) => {
+      el.addEventListener("click", async (event) => {
+        const noteId = event.target.dataset.id;
+
+        try {
+          const note = await NotesApi.getById(noteId);
+          await BookmarkNotesIdb.putBookmark(note.data);
+
+          this._initialData();
+        } catch (error) {
+          console.error("Something wrong when bookmarking a note");
+          console.error(error);
+        }
+      });
+    });
+
+    //* Add event listener to delete button for each note item
+    containerEl.querySelectorAll(`#removeBookmarkButton`).forEach((el) => {
+      el.addEventListener("click", async (event) => {
+        const noteId = event.target.dataset.id;
+
+        try {
+          await BookmarkNotesIdb.deleteBookmark(noteId);
+
+          this._initialData();
+        } catch (error) {
+          console.error(
+            "Something wrong when deleting bookmark note from IndexedDB"
+          );
+          console.error(error);
         }
       });
     });
